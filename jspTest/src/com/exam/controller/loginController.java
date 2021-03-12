62ab60a627e7fd12236320bb9132f7e39127b65c
ewhdtls0 <46704442+ewhdtls0@users.noreply.github.com>
2021-03-12 10:50:58 +0900

    loginController

---
+++ jspTest/src/com/exam/controller/loginController.java
@@ -0,0 +1,44 @@
+package com.exam.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.exam.dao.userDAO;
+
+public class loginController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+		rd.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String id = req.getParameter("id");
+		String pw = req.getParameter("pw");
+		
+		userDAO uDAO = userDAO.getInstance();
+		int loginResult = uDAO.login(id, pw);
+		
+		if (loginResult == 1) {
+			req.setAttribute("loginResult", loginResult);
+			HttpSession session = req.getSession();
+			session.setAttribute("sessionID", id);
+			RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+			rd.forward(req, resp);
+		} else {
+			req.setAttribute("loginResult", loginResult);
+			RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+			rd.forward(req, resp);
+		}
+	}
+
+}
